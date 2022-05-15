08ccaf93cf035d413236746662a5465ae7c83a8b
BrianWangila <bwanjalah@gmail.com>
2022-05-15 15:59:15 +0300

    edit the if statement on the second MCQ

--- Assets/JS/code.js
+++ Assets/JS/code.js
@@ -3,8 +3,17 @@ function result (){
   if (document.getElementById("correct1").checked){
     score+=20;
   }
-  if (document.getElementById("correct2").checked){
-    score+=20;
+  if (document.getElementById("correct2a").checked){
+    score+=5;
+  }
+  if (document.getElementById("correct2b").checked){
+    score+=5;
+  }
+  if (document.getElementById("correct2c").checked){
+    score+=5;
+  }
+  if (document.getElementById("correct2d").checked){
+    score+=5;
   }
   if (document.getElementById("correct3").checked){
     score+=20;
