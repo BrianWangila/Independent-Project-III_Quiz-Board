4b44ee9e273937eb6bf20a9feddfd9997c73bba6
BrianWangila <bwanjalah@gmail.com>
2022-05-15 16:17:00 +0300

    changed marks from 20 to 10

--- Assets/JS/code.js
+++ Assets/JS/code.js
@@ -1,31 +1,33 @@
 function result (){
   var score=0;
   if (document.getElementById("correct1").checked){
-    score+=20;
+    score+=10;
   }
   if (document.getElementById("correct2a").checked){
-    score+=5;
+    score+=2.5;
   }
   if (document.getElementById("correct2b").checked){
-    score+=5;
+    score+=2.5;
   }
   if (document.getElementById("correct2c").checked){
-    score+=5;
+    score+=2.5;
   }
   if (document.getElementById("correct2d").checked){
-    score+=5;
+    score+=2.5;
   }
   if (document.getElementById("correct3").checked){
-    score+=20;
+    score+=10;
   }
   if (document.getElementById("correct4").checked){
-    score+=20;
+    score+=10;
   }
   if (document.getElementById("correct5").checked){
-    score+=20;
+    score+=10;
   }
-  document.write("Your Score is: " + score)
+  var total = (score/50)*100;
+  document.write("Your Score is: " + total + "%")
 }
+
 
 // function next(){
 //   window.location = "Assets/Pages/Index.html"
