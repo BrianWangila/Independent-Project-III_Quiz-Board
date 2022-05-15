a54a31c11d63d2787891a02a1f94cbec4c115bb3
BrianWangila <bwanjalah@gmail.com>
2022-05-15 15:56:09 +0300

    add onclick property on the Start test button that links to js, change id to class in the MCQs and add id of the correct answers, and input if statement on the function result

--- Assets/JS/code.js
+++ Assets/JS/code.js
@@ -1,1 +1,23 @@
-function result
+function result (){
+  var score=0;
+  if (document.getElementById("correct1").checked){
+    score+=20;
+  }
+  if (document.getElementById("correct2").checked){
+    score+=20;
+  }
+  if (document.getElementById("correct3").checked){
+    score+=20;
+  }
+  if (document.getElementById("correct4").checked){
+    score+=20;
+  }
+  if (document.getElementById("correct5").checked){
+    score+=20;
+  }
+  document.write("Your Score is: " + score)
+}
+
+// function next(){
+//   window.location = "Assets/Pages/Index.html"
+// }
